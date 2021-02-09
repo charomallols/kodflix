ff5ffb099b2e40cb328378fc8d92dac779f79772
Charo Mallols <charo.mallols@gmail.com>
2021-02-09 10:01:15 +0100

    último saludo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome again!
+          Welcome una vez más
         </p>
         <a
           className="App-link"
