e3f8859af4c4286c4acef6e56b163bf72136a152
Charo Mallols <charo.mallols@gmail.com>
2021-02-25 10:06:12 +0100

    kodflix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Technologies from './Technologies';
 
 import './App.css';
 
-
 class App extends Component {
   render() {
     return (
+    <Router>
       <div className="App">
-        <br /><br /><br />
-        <Technologies />
+        <Route exact path='/' component={Technologies} />
+        <Route exact path='/details' component={Details} />
       </div>
+    </Router>
     );
   }
 }
 
+function Details() {
+  return (
+    <div><h1>Welcome to the details page, WIP :)</h1></div>
+  )
+}
 
 export default App;
 
